8dc249410f239ffaef19befae740658cae44e4a7
emil-nabil333 <elbrinselomda@yahoo.com>
2025-03-12 19:59:02 +0200

    Add files via upload

--- skin-Youchie-OBH-FHD.sh
+++ skin-Youchie-OBH-FHD.sh
@@ -1,57 +1,34 @@
 #!/bin/bash
-######################################################################################
-## Command=wget https://raw.githubusercontent.com/emilnabil/skins-obh/refs/heads/main/skin-Youchie-OBH-FHD.sh -O - | /bin/sh
-##
-###########################################
-###########################################
 
-# My config script #
-PLUGIN_TAR="skin-Youchie-PLI-FHD.tar.gz"
-MY_URL="https://raw.githubusercontent.com/emilnabil/skins-obh/refs/heads/main"
+echo "Removing previous version of Youchie-FHD..."
+sleep 2
 
-######################################################################################
-MY_EM='============================================================================================================'
-
-# Remove Old Plugin
-echo "   >>>>   Removing old version..."
-rm -rf /usr/share/enigma2/Youchie-FHD
-
-echo "============================================================================================================================"
-echo " DOWNLOAD AND INSTALL PLUGIN "
-echo "   Installing plugin, please wait..."
-
-cd /tmp || exit 1
-set -e
-
-wget "$MY_URL/$PLUGIN_TAR"
-sleep 1
-tar xzvpf "/tmp/$PLUGIN_TAR" -C /
-rm -rf "/tmp/$PLUGIN_TAR"
-
-echo "================================="
-set +e
-
-if [ $? -eq 0 ]; then
-    echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+# Check if the directory exists before removing it
+if [ -d /usr/share/enigma2/Youchie-FHD ]; then
+    rm -rf /usr/lib/enigma2/python/Plugins/Extensions/YouchieSettings > /dev/null 2>&1
+    
+    echo 'Package removed.'
+else
+    echo "You do not have previous version"
 fi
 
-echo "********************************************************************************"
-echo "   UPLOADED BY  >>>>   EMIL_NABIL " 
-sleep 4                        
-echo "$MY_EM"
-echo "**********************************************************************************"
+echo ""
+opkg install enigma2-plugin-extensions-bitrate enigma2-plugin-extensions-oaweather
+opkg install curl
+sleep 2
 
-exit 0
+# Download and extract the package
+cd /tmp || exit
+curl -k -Lbk -m 55532 -m 555104 "https://github.com/emilnabil/skins-obh/raw/refs/heads/main/skin-Youchie-OBH-FHD.tar.gz" -o /tmp/skin-Youchie-OBH-FHD.tar.gz
+sleep 1
+echo "Installing ...."
+tar -xzf /tmp/skin-Youchie-OBH-FHD.tar.gz -C /
+echo ""
+echo ""
+sleep 1
+rm -f /tmp/skin-Youchie-OBH-FHD.tar.gz
 
-
-
-
-
-
-
-
-
-
+exit 
 
 
 
